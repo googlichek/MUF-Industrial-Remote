097addbe2c1fe0d34988eb38408fcf2eedb816cc
Goga Akobiya <googlichek.one@gmail.com>
2017-07-03 15:00:41 +0300

    muf-industrial: Editing remote controller.

--- Assets/Scripts/LoopHandler.cs
+++ Assets/Scripts/LoopHandler.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 public class LoopHandler : MonoBehaviour
 {
 	public float ScaleModificator;
+	public float Delay = 0;
 
 	private void Start()
 	{
-		gameObject.transform.DOScale(ScaleModificator, 1f).SetEase(Ease.InOutQuad).SetLoops(-1, LoopType.Yoyo);
+		gameObject.transform.DOScale(ScaleModificator, 1f).SetEase(Ease.InOutFlash).SetLoops(-1, LoopType.Yoyo).SetDelay(Delay);
 	}
 }
